9cce384b77a2c97866a0ab732b9fd317eb440aab
HienNguyen102 <nguyenconghien0102@gmail.com>
2017-02-19 08:16:12 -0800

    them secret de deploy

--- config/main.js
+++ config/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'secret': 'longobnoxiouspassphrase',
+  'secret': '6h2GTKs8tMH9_JVg69guZM800Sigyw34',
   'database': 'mongodb://<dbuser>:<dbpassword>@ds157459.mlab.com:57459/heroku_3fxvjrvn'
 };
